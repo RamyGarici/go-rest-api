0aafdf0564b449ebe49f7f1dbe983c810186f9e1
RamyGarici <ramymanilgarici@gmail.com>
2026-07-28 09:11:41 +0100

    add and get notes

--- notes-app/main.go
+++ notes-app/main.go
@@ -1,7 +1,11 @@
 package main
-import("github.com/gin-gonic/gin"
-"net/http"
-"time")
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
 
 
 type note struct{
@@ -12,14 +16,33 @@ type note struct{
 	UpdatedAt time.Time `json:"updated_at"`
 
 }
+var notes []note
 
+func getNotes(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK,notes)
+}
 
+func addNotes(c *gin.Context) {
+	var newNote note
+	err := c.BindJSON(&newNote)
+	if err != nil{
+		c.IndentedJSON(http.StatusBadRequest,gin.H{"message":"Error adding the note"})
+		return
+	}
+	notes = append(notes, newNote)
+	c.IndentedJSON(http.StatusCreated,newNote)
+}
 
 
 
 
 
 func main() {
+	router := gin.Default()
+	router.GET("/notes",getNotes)
+	router.POST("/notes",addNotes)
+	// router.GET("/notes/:id",getNote)
+
 
 
 
