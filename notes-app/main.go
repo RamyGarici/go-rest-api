f22037c39d4fe9fb282eee73d7485bca54210397
RamyGarici <ramymanilgarici@gmail.com>
2026-07-28 09:35:28 +0100

    get notes by id

--- notes-app/main.go
+++ notes-app/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"net/http"
 	"time"
+	"errors"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,8 +12,8 @@ import (
 
 type note struct{
 	ID int `json:"id"`
-	Title string `json:"title"`
-	Content string `json:"content"`
+	Title string `json:"title" binding:"required"`
+	Content string `json:"content" binding:"required"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 
@@ -26,11 +28,40 @@ func addNotes(c *gin.Context) {
 	var newNote note
 	err := c.BindJSON(&newNote)
 	if err != nil{
-		c.IndentedJSON(http.StatusBadRequest,gin.H{"message":"Error adding the note"})
+		c.IndentedJSON(http.StatusBadRequest,gin.H{"message":"Title and Content required"})
 		return
 	}
+	newNote.ID = len(notes)+1
+	
+	newNote.CreatedAt = time.Now()
+	newNote.UpdatedAt = time.Now()
 	notes = append(notes, newNote)
 	c.IndentedJSON(http.StatusCreated,newNote)
+}
+
+func getNote(c *gin.Context) {
+	id,err := strconv.Atoi(c.Param("id"))
+	if err != nil{
+		c.IndentedJSON(http.StatusBadRequest,gin.H{"message":"invalid id"})
+		return
+	}
+	note, err := getNoteById(id)
+	if err !=nil{
+		c.IndentedJSON(http.StatusNotFound,gin.H{"message":"Note not found"})
+		return
+	}
+	c.IndentedJSON(http.StatusOK,note)
+}
+
+func getNoteById(id int) (*note, error){
+	for i,n := range notes{
+		if n.ID == id{
+			return &notes[i],nil
+		}
+	}
+	err := errors.New("Note not found")
+	return nil,err
+
 }
 
 
@@ -41,8 +72,9 @@ func main() {
 	router := gin.Default()
 	router.GET("/notes",getNotes)
 	router.POST("/notes",addNotes)
-	// router.GET("/notes/:id",getNote)
+	router.GET("/notes/:id",getNote)
 
+	router.Run(":8080")
 
 
 
