2f11fc8f918e89a8435635a1a44ebc4a123387cd
RamyGarici <ramymanilgarici@gmail.com>
2026-07-29 11:39:59 +0100

    fetching data from a weather api

---
+++ weather-app/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+type weatherResponse struct {
+	Main    mainData `json:"main"`
+	Weather []weather `json:"weather"`
+}
+
+type mainData struct {
+	Temp      float64 `json:"temp"`
+	FeelsLike float64 `json:"feels_like"`
+}
+
+type weather struct {
+	Description string `json:"description"`
+}
+
+func main() {
+	client := &http.Client{Timeout: time.Second * 10}
+	var response weatherResponse
+
+	req, err := http.NewRequest(http.MethodGet, "https://api.openweathermap.org/data/2.5/weather?lat=48.85&lon=2.35&appid=b73142ec6a46fe59f577ee9fcd7b6182&units=metric", nil)
+	if err != nil {
+		fmt.Println("Error creating request:", err)
+		return
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error making request:", err)
+		return
+	}
+	defer resp.Body.Close()
+	decoder := json.NewDecoder(resp.Body)
+	if err := decoder.Decode(&response); err != nil {
+		fmt.Println("error decoding response body")
+		return
+	}
+fmt.Println(response.Main.Temp)
+fmt.Println(response.Main.FeelsLike)
+
+
+fmt.Println(response.Weather[0].Description)
+
+}
