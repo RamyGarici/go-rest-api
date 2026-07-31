b0e32dda10d88ae41123d5e5db5f2bcede4519dd
RamyGarici <ramymanilgarici@gmail.com>
2026-07-31 18:50:07 +0100

    shorten urls

--- url-shortener/main.go
+++ url-shortener/main.go
@@ -1,11 +1,14 @@
-package main 
+package main
 
 import (
-    "github.com/jackc/pgx/v5"
-    "github.com/gin-gonic/gin"
-    "time"
-    "net/http"
-    "net/url"
+	"context"
+	"net/http"
+	"net/url"
+	"os"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5"
 )
 
 type URL struct{
@@ -15,32 +18,92 @@ type URL struct{
     CreatedAt time.Time
 
 }
+
 type request struct{
-    Url string `json:"url"`
+    URL string `json:"url"`
+}
+type apiConfig struct {
+    DB *pgx.Conn
 }
 
-func shortenURL(c *gin.Context) {
-    var request body
+func(cfg *apiConfig) shortenURL(c *gin.Context) {
+    var request request
     err := c.ShouldBindJSON(&request)
 	if err!=nil{
 		c.IndentedJSON(http.StatusBadRequest,gin.H{"message":"Invalid body"})
         return
 	}
-    _,err = url.ParseRequestURI(request.Url)
+    _,err = url.ParseRequestURI(request.URL)
     if err != nil{
         c.IndentedJSON(http.StatusBadRequest,gin.H{"message":"Invalid URL"})
         return
     }
+    var id int
+    err = cfg.DB.QueryRow(
+        c.Request.Context(),
+        "INSERT INTO urls(original_url) VALUES ($1) RETURNING id",
+        request.Url,
+    ).Scan(&id)
+
+    if err!=nil{
+        c.JSON(http.StatusInternalServerError,gin.H{"message":"Database error"})
+        return
+    }
+    result := convertToBase62(id)
+   _, err = cfg.DB.Exec(
+    c.Request.Context(),
+    "UPDATE urls SET short_code = $1 WHERE id = $2",
+    result,
+    id,
+)
+if err!=nil{
+        c.JSON(http.StatusInternalServerError,gin.H{"message":"Database error"})
+        return
+    }
+   c.IndentedJSON(http.StatusOK,gin.H{"short_url": "http://localhost:8080/"+result})
 
     
 
 }
- 
+ func convertToBase62(id int)string{
+
+    chars := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    result := ""
+    for id > 0 {
+        remainder := id % 62
+       
+        char := chars[remainder]
+         result = string(char) + result
+        id = id/62
+
+
+}
+    return result
+ }
+
 
 
 
 func main() {
+ connStr := os.Getenv("DATABASE_URL")
+  conn, err := pgx.Connect(context.Background(),connStr)
+ if err!=nil{
+    return
+ }
+ cfg := apiConfig{
+    DB: conn,
+ }
+
+ defer conn.Close(context.Background())
+
+
+
+
+
+
+
+
    router := gin.Default()
-   router.POST("/shorten",shortenURL)
+   router.POST("/shorten",cfg.shortenURL)
    router.Run(":8080")
 }
